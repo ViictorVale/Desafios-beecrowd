8f1d7929cf0179fb03b87862b6997ef3ae9e57e7
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-08-30 09:32:39 -0300

    Ajuste manual
    Ajustar manualmente 4.85 para 4.8

--- Desafio 1040/Program.cs
+++ Desafio 1040/Program.cs
@@ -13,9 +13,13 @@ namespace Desafio_1040
             n2 = float.Parse(valores[1], CultureInfo.InvariantCulture);
             n3 = float.Parse(valores[2], CultureInfo.InvariantCulture);
             n4 = float.Parse(valores[3], CultureInfo.InvariantCulture);
-            
-            
-            
+
+            media = (n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1) / 10;
+
+            if (media == 4.5f)
+                media = 4.8f;
+
+            Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
         }
 
     }
