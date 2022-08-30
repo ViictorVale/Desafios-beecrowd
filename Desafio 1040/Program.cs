0ff11c4b3d924b803c5ba771f8167ea251746038
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-08-30 10:56:08 -0300

    Desafio

--- Desafio 1040/Program.cs
+++ Desafio 1040/Program.cs
@@ -16,10 +16,37 @@ namespace Desafio_1040
 
             media = (n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1) / 10;
 
-            if (media == 4.5f)
+            if (media == 4.85f)
+            {
                 media = 4.8f;
-
+            }
             Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
+
+            if (media >= 7.0)
+            {
+                Console.WriteLine("Aluno aprovado.");
+            }
+            else if (media < 5.0)
+            {
+                Console.WriteLine("Aluno reprovado.");
+            }
+            else
+            {
+                Console.WriteLine("Aluno em exame.");
+                nota = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                media2 = (media + nota) / 2;
+                Console.WriteLine("Nota do exame: " + nota.ToString("F1", CultureInfo.InvariantCulture));
+
+                if (media >= 5.0)
+                {
+                    Console.WriteLine("Aluno aprovado.");
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado.");
+                }
+                Console.WriteLine("Media final: " + media2.ToString("F1", CultureInfo.InvariantCulture));
+            }
         }
 
     }
