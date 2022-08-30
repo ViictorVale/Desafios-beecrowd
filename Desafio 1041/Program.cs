32c6d5addc8144e954e9bd06da8bd88c56f6020b
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-08-30 20:20:23 -0300

    Desafio

---
+++ Desafio 1041/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+namespace Desafio_1041
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y;
+
+            string[] qd = Console.ReadLine().Split(' ');
+            x = float.Parse(qd[0], CultureInfo.InvariantCulture);
+            y = float.Parse(qd[1], CultureInfo.InvariantCulture);
+
+            if (x == 0 && y == 0) 
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x < 0 && y >= 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (x >= 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if (x >= 0 && y >= 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else
+            {
+                Console.WriteLine("Eixo Y");
+            }
+        }
+    }
+
+}
