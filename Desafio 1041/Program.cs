1c1692a3a8a87a2cfcfeba7212aef6d4ae2a3382
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-08-30 20:40:05 -0300

    Desafio

--- Desafio 1041/Program.cs
+++ Desafio 1041/Program.cs
@@ -12,33 +12,33 @@ namespace Desafio_1041
             x = float.Parse(qd[0], CultureInfo.InvariantCulture);
             y = float.Parse(qd[1], CultureInfo.InvariantCulture);
 
-            if (x == 0 && y == 0) 
+            if (x == 0 && y == 0.0) 
             {
                 Console.WriteLine("Origem");
             }
-            else if (x < 0 && y >= 0)
+            else if (x == 0.0)
             {
-                Console.WriteLine("Q2");
+                Console.WriteLine("Eixo Y");
             }
-            else if (x < 0 && y < 0)
-            {
-                Console.WriteLine("Q3");
-            }
-            else if (x >= 0 && y < 0)
-            {
-                Console.WriteLine("Q4");
-            }
-            else if (x >= 0 && y >= 0)
-            {
-                Console.WriteLine("Q1");
-            }
-            else if (x == 0)
+            else if (y == 0.0)
             {
                 Console.WriteLine("Eixo X");
             }
+            else if (x > 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0.0 && y < 0.0)
+            {
+                Console.WriteLine("Q3");
+            }
             else
             {
-                Console.WriteLine("Eixo Y");
+                Console.WriteLine("Q4");
             }
         }
     }
