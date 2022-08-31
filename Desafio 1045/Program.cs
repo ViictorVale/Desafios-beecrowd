81f97e530c868d2b2124a9c0b5ac40c93d38ef9e
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-08-31 10:20:15 -0300

    Desafio

---
+++ Desafio 1045/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace Desafio_1045
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3,
+                 a, b, c;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            n1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            n2 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            n3 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            if (n1 > n2 && n1 > n3)
+                a = n1;
+                if (n2 > n3)
+                {
+
+                }
+            }
+        }
+    }
+}
