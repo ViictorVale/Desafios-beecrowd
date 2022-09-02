9ab08f536838ea595e0c9ab5a48bca4ca2c5ec66
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-02 10:39:20 -0300

    Desafios

--- Desafio 1045/Program.cs
+++ Desafio 1045/Program.cs
@@ -15,17 +15,78 @@ namespace Desafio_1045
             n3 = double.Parse(valores[2], CultureInfo.InvariantCulture);
 
             if (n1 > n2 && n1 > n3)
-                a = n1;
-            if (n2 > n3)
             {
-                b = n2;
-                c = n3;
+                a = n1;
+                if (n2 > n3)
+                {
+                    b = n2;
+                    c = n3;
+                }
+                else
+                {
+                    b = n3;
+                    c = n2;
+                }
+            }
+            else if (n2 > n3)
+            {
+                a = n2;
+                if (n1 > n3)
+                {
+                    b = n1;
+                    c = n3;
+                }
+                else
+                {
+                    b = n3;
+                    c = n1;
+                }
             }
             else
             {
-                b = 
+                a = n3;
+                if (n1 > n2)
+                {
+                    b = n1;
+                    c = n2;
+                }
+
+                else
+                {
+                    b = n2;
+                    c = n1;
+                }
+            }
+            if (a >= b + c)
+            {
+                Console.WriteLine("NÃO FORMA TRIANGULO");
+            }
+            else
+            {
+                if (a * a > b * b + c * c)
+                {
+                    Console.WriteLine("TRIANGULO RETANGULO");
+                }
+
+                else if (a * a > b * b + c * c)
+                {
+                    Console.WriteLine("TRIANGULO OBTUSANGULO");
+                }
+                else
+                {
+                    Console.WriteLine("TRIANGULO ACUTANGULO");
+                }
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("TRIANGULO EQUILATERO");
+                }
+                else if (a == b || a == c || b == c)
+                {
+                    Console.WriteLine("TRIANGULO ISOSCELES");
+                }
             }
         }
     }
 }
-}
+
+
