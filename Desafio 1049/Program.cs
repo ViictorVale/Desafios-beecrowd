05271eec9591778505293098b1e2566df73c9240
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-04 12:42:35 -0300

    Desafios

---
+++ Desafio 1049/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Desafio_1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filo = Console.ReadLine();
+            string classe = Console.ReadLine();
+            string dieta = Console.ReadLine();
+
+            if (filo == "vertebrado")
+            {
+                if ( classe == "ave")
+                {
+                   if (dieta == "carnivoro")
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                   else
+                    {
+                        Console.WriteLine("pomba");
+                    }
+                }
+                else
+                {
+                    if (dieta == "onivoro")
+                    {
+                        Console.WriteLine("homem");
+                    }
+                    else
+                    {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+            else
+            {
+                if (classe == "inseto")
+                {
+                    if (dieta == "hematofago")
+                    {
+                        Console.WriteLine("pulga");
+                    }
+                    else
+                    {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                else
+                {
+                    if (dieta == "hematofago")
+                    {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else
+                    {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
+        }
+    }
+}
