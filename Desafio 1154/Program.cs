afd5df5befbbdaaa62f9f816489ada7d0076068d
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-07 21:12:03 -0300

    Desafios

---
+++ Desafio 1154/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace Desafio_1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade, soma, qd;
+            idade = int.Parse(Console.ReadLine());
+            soma = 0;
+            qd = 0;
+            while(idade > 0)
+            {
+                qd = qd + 1;
+                soma = soma + idade;
+                idade = int.Parse(Console.ReadLine());
+            }
+            double media = soma / qd;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
