361a66ece4ca17a7a5bd1982fdc76ce75a550b7f
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-09 11:02:35 -0300

    Desafios

---
+++ Desafio 1159/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace uri1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+
+            while (x != 0)
+            {
+
+                // se for impar, some mais 1
+                if (x % 2 != 0)
+                {
+                    x = x + 1;
+                }
+
+                int soma = 5 * x + 20;
+                Console.WriteLine(soma);
+
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
