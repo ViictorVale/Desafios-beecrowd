3ffa7dd42556c2a1e941e754f310ec66af4f62c6
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-13 20:50:13 -0300

    Desafios

---
+++ Desafio 1074/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Desafio_1074
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                if (x == 0)
+                {
+                    Console.WriteLine("NULL");
+                }
+                else if (x < 0)
+                {
+                    if (x % 2 != 0)
+                    {
+                        Console.WriteLine("ODD NEGATIVE");
+                    }
+                    else
+                    {
+                        Console.WriteLine("EVEN NEGATIVE");
+                    }
+                }
+                else
+                {
+                    if (x % 2 != 0)
+                    {
+                        Console.WriteLine("ODD POSITIVE");
+                    }
+                    else
+                    {
+                        Console.WriteLine("EVEN POSITIVE");
+                    }
+                }
+            }
+        }
+    }
+}
