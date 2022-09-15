1c401c58190ba7aca798ca707aebd0948b25d891
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-15 09:54:16 -0300

    Desafios

---
+++ Desafio 1080/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Desafio_1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = int.Parse(Console.ReadLine());
+            int posicao = 1;
+            for (int i = 2; i <= 100; i++)
+            {
+                int valores = int.Parse(Console.ReadLine());
+                if (valores > maior)
+                {
+                    maior = valores;
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+        }
+    }
+}
