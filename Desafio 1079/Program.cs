a1e3ed676deb7e4a8cd9226546f731fb4e28589a
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-14 21:03:06 -0300

    Desafios

---
+++ Desafio 1079/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio_1079
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                double v1 = double.Parse(vet[0], CultureInfo.InvariantCulture);
+                double v2 = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                double v3 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+                double calc = (v1 * 2.0 + v2 * 3.0 + v3 * 5.0) / 10;
+                Console.WriteLine(calc.ToString("F1", CultureInfo.InvariantCulture));
+                
+            }
+        }
+    }
+}
