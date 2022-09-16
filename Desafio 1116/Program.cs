4e3f172fe7136cd11a4b7c973a54c1335d590d2f
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-16 20:46:23 -0300

    Desafios

---
+++ Desafio 1116/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio_1116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string[] num = Console.ReadLine().Split(' ');
+                int x = int.Parse(num[0]);
+                int y = int.Parse(num[1]);
+
+                if (y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    double div = (double)x / y;
+                    Console.WriteLine(div.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
