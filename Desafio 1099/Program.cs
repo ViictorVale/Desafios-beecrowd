07d787ff6da56571c517e2b09effe8745d83c47a
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-17 20:07:33 -0300

    Desafios

---
+++ Desafio 1099/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Desafio_1099
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] num = Console.ReadLine().Split(' ');
+                int x = int.Parse(num[0]);
+                int y = int.Parse(num[1]);
+                
+                if(x > y)
+                {
+                    int aux = x;
+                    x = y;
+                    y = aux;
+                }
+                int soma = 0;
+                for (int j = x + 1; j < y; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        soma = soma + j;
+                    }
+                }
+                Console.WriteLine(soma);
+            }
+
+        }
+    }
+}
